6fd16830ea8e380c976260c740688074ff0efa8c
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-09-13 09:29:34 +0300

    Working with responsive mode

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 lucide.createIcons();
+
+window.onload = () => {
+  const filterSideBar = document.querySelector("#filter_sidebar");
+  const quickLinks = document.querySelector("#quick_links");
+  const banner = document.querySelector("#banner");
+  document.querySelector("#show_filter").addEventListener("click", () => {
+    filterSideBar.classList.toggle("max-lg:-translate-x-[calc(100%+20px)]");
+  });
+
+  document.querySelector("#show_links").addEventListener("click", () => {
+    quickLinks.classList.toggle("mt-3")
+    quickLinks.classList.toggle("max-xl:h-0");
+  });
+};
